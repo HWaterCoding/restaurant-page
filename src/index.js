044a16eb12f46ab6b647f573ac80f2e5cef9e1eb
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-02-19 14:39:26 -0500

    Finish rough draft of styling for homepage Add JS pseudocode

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 console.log("Test")
 
+//This will be the main file that controls tab-switching logic. 
+//Create a new file for each module to represent a single tab and it's content-creation
+//This file will simply add event listeners to the three tab buttons
+//When one button is clicked it will wipe out the content current on the page and then
+//it will run the corresponding tab module to replace the content with what was clicked.
+
